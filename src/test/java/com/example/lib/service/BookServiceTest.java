b6cdd2b5a1d7348cee10290fe4fa18b67064bf19
furkanozmen <ozmenf97@gmail.com>
2022-09-21 23:54:04 +0300

    selam ben bir commitim

---
+++ src/test/java/com/example/lib/service/BookServiceTest.java
@@ -0,0 +1,13 @@
+package com.example.lib.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceTest {
+    @Test
+    void test() throws Exception {
+        throw new Exception("patladım");
+    }
+
+}
