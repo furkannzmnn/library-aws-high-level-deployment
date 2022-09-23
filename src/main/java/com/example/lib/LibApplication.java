e064c945dc6437c26a872eadb7207eb425336cff
furkanozmen <ozmenf97@gmail.com>
2022-09-23 18:49:44 +0300

    SUCESS TEST

--- src/main/java/com/example/lib/LibApplication.java
+++ src/main/java/com/example/lib/LibApplication.java
@@ -1,6 +1,10 @@
 package com.example.lib;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -25,4 +29,16 @@ public class LibApplication {
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
     }
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String description,
+                                 @Value("${application-version}") String version){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ECOMMERCE API")
+                        .version(version)
+                        .description(description)
+                        .license(new License().name("BLOG API Licence")));
+    }
+
 }
