c5558e98851342c212fc45f35344ff41caca82c9
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-27 18:24:04 +0300

    AuthService created

---
+++ src/main/java/com/example/lib/service/AuthService.java
@@ -0,0 +1,44 @@
+package com.example.lib.service;
+
+import com.example.lib.dto.ErrorCode;
+import com.example.lib.dto.TokenResponseDTO;
+import com.example.lib.dto.UserDto;
+import com.example.lib.exception.GenericException;
+import com.example.lib.request.LoginRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final TokenService tokenService;
+
+
+    public TokenResponseDTO login(LoginRequest loginRequest) {
+        try {
+            Authentication auth = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+            return TokenResponseDTO
+                    .builder()
+                    .accessToken(tokenService.generateToken(auth))
+                    .user(userService.getUserDto(loginRequest.getUsername()))
+                    .build();
+        } catch (final BadCredentialsException badCredentialsException) {
+            throw GenericException.builder().httpStatus(HttpStatus.NOT_FOUND).errorCode(ErrorCode.USER_NOT_FOUND).errorMessage("Invalid Username or Password").build();
+        }
+    }
+
+    public UserDto getAuthenticatedUser() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userService.getUserDto(username);
+    }
+}
