67c3c6185990ebce33c7ead12afebd1bc911ee31
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-27 18:22:38 +0300

    UserRepository updated

--- src/main/java/com/example/lib/repository/UserRepository.java
+++ src/main/java/com/example/lib/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.example.lib.repository;
 import com.example.lib.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    Optional<User> findByUsername(String username);
 }
