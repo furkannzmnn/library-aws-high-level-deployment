fe283b0bfe9beb5934d41ce4593eeea1e30063f5
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-27 18:24:27 +0300

    AuthController and LoginRequest model created

---
+++ src/main/java/com/example/lib/api/AuthController.java
@@ -0,0 +1,26 @@
+package com.example.lib.api;
+
+import com.example.lib.dto.TokenResponseDTO;
+import com.example.lib.request.LoginRequest;
+import com.example.lib.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponseDTO> authenticate(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.login(loginRequest));
+    }
+
+
+}
---
+++ src/main/java/com/example/lib/request/LoginRequest.java
@@ -0,0 +1,17 @@
+package com.example.lib.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class LoginRequest {
+
+    private String username;
+    private String password;
+
+}
