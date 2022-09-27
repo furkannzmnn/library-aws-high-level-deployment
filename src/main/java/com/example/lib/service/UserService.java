e3cf926c00fe39976824a63ee889d8a3569828fa
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-27 18:23:10 +0300

    UserService created

---
+++ src/main/java/com/example/lib/service/UserService.java
@@ -0,0 +1,42 @@
+package com.example.lib.service;
+
+
+import com.example.lib.dto.UserDto;
+import com.example.lib.exception.GenericException;
+import com.example.lib.model.User;
+import com.example.lib.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> GenericException.builder().httpStatus(HttpStatus.NOT_FOUND).errorMessage("user not found!").build());
+    }
+
+    public UserDto getUserDto(String username) {
+        var user = findUserByUsername(username);
+        return UserDto.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .role(user.getRole())
+                .build();
+    }
+
+}
