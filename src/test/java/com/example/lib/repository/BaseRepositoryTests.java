e2ea3279a7546192bbd19a6b1436bbaa11da5c08
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-10-07 22:06:09 +0300

    BaseRepositoryTest ActiveProfiles Integration Tanımlandı

--- src/test/java/com/example/lib/repository/BaseRepositoryTests.java
+++ src/test/java/com/example/lib/repository/BaseRepositoryTests.java
@@ -3,10 +3,11 @@ package com.example.lib.repository;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles(value = "integration")
 @ExtendWith(SpringExtension.class)
 public abstract class BaseRepositoryTests {
 }
