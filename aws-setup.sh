6171a9be68713bb795397009fc0ffb7ca9d35f91
Furkan Özmen <31289019+furkannzmnn@users.noreply.github.com>
2022-10-17 09:59:36 +0300

    secret manager impl and s3 url added

--- aws-setup.sh
+++ aws-setup.sh
@@ -1,1 +1,6 @@
 aws --endpoint-url=http://localhost:4566 secretsmanager create-secret --name aws/secret --secret-string '{"my_uname":"username","my_pwd":"password"}'
+
+aws --endpoint-url=http://localhost:4566  s3api create-bucket \
+              --bucket library-folksdev \
+              --region eu-west-1 \
+              --create-bucket-configuration LocationConstraint=eu-west-3
