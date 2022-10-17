a5f0be38f516922d4d136d4102016e1e229ec3f1
Furkan Özmen <31289019+furkannzmnn@users.noreply.github.com>
2022-10-17 10:04:33 +0300

    secret manager impl and s3 url added

--- aws-setup.sh
+++ aws-setup.sh
@@ -1,3 +1,8 @@
+
+## if aws cli is not installed, install it
+## https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
+
+
 aws --endpoint-url=http://localhost:4566 secretsmanager create-secret --name aws/secret --secret-string '{"my_uname":"username","my_pwd":"password"}'
 
 aws --endpoint-url=http://localhost:4566  s3api create-bucket \
