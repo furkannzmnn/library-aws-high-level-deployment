6934c75caea75c2da5b42555a7a458d06192da1e
Furkan Özmen <31289019+furkannzmnn@users.noreply.github.com>
2022-10-17 18:54:08 +0300

    secret manager impl and s3 url added

--- aws-setup.sh
+++ aws-setup.sh
@@ -2,6 +2,10 @@
 ## if aws cli is not installed, install it
 ## https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
 
+aws configure set aws_access_key_id "test"
+aws configure set aws_secret_access_key "test"
+aws configure set default.region eu-west-3
+
 
 aws --endpoint-url=http://localhost:4566 secretsmanager create-secret --name aws/secret --secret-string '{"my_uname":"username","my_pwd":"password"}'
 
