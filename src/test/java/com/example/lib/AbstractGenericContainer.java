412d3885fbb894460f0a8a6aa39ebe5101c905ae
Furkan Özmen <31289019+furkannzmnn@users.noreply.github.com>
2022-10-22 13:40:10 +0300

    implementation localstack testcontainers

--- src/test/java/com/example/lib/AbstractGenericContainer.java
+++ src/test/java/com/example/lib/AbstractGenericContainer.java
@@ -1,16 +1,18 @@
-package com.example.lib.api;
+package com.example.lib;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.support.TestPropertySourceUtils;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
 
 import java.io.IOException;
 
 @ContextConfiguration(initializers = AbstractGenericContainer.Initializer.class)
-public class AbstractGenericContainer {
+public abstract class AbstractGenericContainer {
     public static final String LOCALSTACK_HOSTNAME = "localhost";
     public static final String LOCALSTACK_REGION = "eu-west-3";
     public static final String LOCALSTACK_ACCESS_KEY = "test";
@@ -19,7 +21,7 @@ public class AbstractGenericContainer {
     // aws config
     public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
         @Override
-        public void initialize(ConfigurableApplicationContext applicationContext) {
+        public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
             String localStackHost;
             try {
                 localStackHost = initializeLocalStack();
@@ -32,21 +34,21 @@ public class AbstractGenericContainer {
 
         private static String initializeLocalStack() throws IOException, InterruptedException {
             localStackContainer.start();
-            localStackContainer.waitingFor(Wait.forHttp("/health").forStatusCode(200));
-            localStackContainer.execInContainer("aws --endpoint-url=http://localhost:4566 secretsmanager create-secret --name aws/secret --secret-string '{\"my_uname\":\"username\",\"my_pwd\":\"password\"}'");
             final Integer mappedPort = localStackContainer.getMappedPort(4566);
-            return "cloud.aws.secrets-manager.end-point.uri=http://localhost:" + mappedPort;
+
+            localStackContainer.execInContainer("aws", "--endpoint-url=http://0.0.0.0:" + 4566, "secretsmanager", "create-secret", "--name", "aws/secret", "--secret-string", "{\"accessKey\":\"" + LOCALSTACK_ACCESS_KEY + "\",\"secretKey\":\"" + LOCALSTACK_SECRET_KEY + "\"}");
+             return "cloud.aws.secrets-manager.end-point.uri=http://localhost:" + mappedPort;
         }
 
-        static LocalStackContainer localStackContainer = new LocalStackContainer()
-                .withServices(LocalStackContainer.Service.S3)
+        static LocalStackContainer localStackContainer = new LocalStackContainer(DockerImageName.parse("localstack/localstack:0.12.17"))
                 .withServices(LocalStackContainer.Service.SECRETSMANAGER)
                 .withExposedPorts(4566, 4566)
-                .waitingFor(Wait.forLogMessage(".*Ready.*", 1))
+                .waitingFor(Wait.forLogMessage("Ready.*", 1))
                 .withEnv("HOSTNAME_EXTERNAL", LOCALSTACK_HOSTNAME)
                 .withEnv("DEFAULT_REGION", LOCALSTACK_REGION)
                 .withEnv("AWS_ACCESS_KEY_ID", LOCALSTACK_ACCESS_KEY)
-                .withEnv("AWS_SECRET_ACCESS_KEY", LOCALSTACK_SECRET_KEY);
+                .withEnv("AWS_SECRET_ACCESS_KEY", LOCALSTACK_SECRET_KEY)
+                .withEnv("PLATFORM", "linux/x86_64");
 
 
     }
--- src/test/java/com/example/lib/api/BaseRestControllerTest.java
+++ src/test/java/com/example/lib/api/BaseRestControllerTest.java
@@ -1,18 +1,9 @@
 package com.example.lib.api;
 
-import org.jetbrains.annotations.NotNull;
+import com.example.lib.AbstractGenericContainer;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.support.TestPropertySourceUtils;
-import org.testcontainers.containers.localstack.LocalStackContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
-
-import java.io.IOException;
 
 @SpringBootTest
 @AutoConfigureMockMvc
